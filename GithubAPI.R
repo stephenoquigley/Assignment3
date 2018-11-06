38c376f11a1ba054efc2e710dee49198c8996c57
Stephen O'Quigley <stephenoquigley@yahoo.ie>
2018-11-06 13:17:12 +0000

    Creating a data frame of following users

--- GithubAPI.R
+++ GithubAPI.R
@@ -9,19 +9,20 @@ library(httr)
 oauth_endpoints("github")
 
 # Change based on what you 
-myapp <- oauth_app(appname = "APIgithub",
+myapp = oauth_app(appname = "APIgithub",
                    key = "90c0d84a80d883a21a2a",
                    secret = "270da448c534eecfefc996e02e20fe35680652c0")
 
 # Get OAuth credentials
-github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+github_token = oauth2.0_token(oauth_endpoints("github"), myapp)
 
 # Use API
-gtoken <- config(token = github_token)
-req <- GET("https://api.github.com/users/stephenoquigley/following", gtoken)
+gtoken = config(token = github_token)
+UserFollowingData = GET("https://api.github.com/users/mbostock/following", gtoken)
+UserFollowingDataContent = content(UserFollowingData)
+UserFollowingDataFrame = jsonlite::fromJSON(jsonlite::toJSON(UserFollowingDataContent))
 
-# Take action on http error
-stop_for_status(req)
+followersLogins = c(UserFollowingDataFrame$login)
 
 # Extract content from a request
 json1 = content(req)
