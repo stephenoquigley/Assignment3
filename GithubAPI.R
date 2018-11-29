e50d237dc5395050eb3361ead28f3dfdefc6041b
Stephen O'Quigley <stephenoquigley@yahoo.ie>
2018-11-29 17:34:06 +0000

    Add search followers function

--- GithubAPI.R
+++ GithubAPI.R
@@ -4,6 +4,8 @@ library(jsonlite)
 library(httpuv)
 #install.packages("httr")
 library(httr)
+detach(package:plotly, unload=TRUE)
+
 
 # Can be github, linkedin etc depending on application
 oauth_endpoints("github")
@@ -15,14 +17,45 @@ myapp = oauth_app(appname = "APIgithub",
 
 # Get OAuth credentials
 github_token = oauth2.0_token(oauth_endpoints("github"), myapp)
+gtoken = config(token = github_token)
+
+getFollowers <- function(username)
+{
+  i = 1
+  x = 1
+  followersDataFrame <- data_frame()
+  while(x!=0)
+  {
+    followers <- GET( paste0("https://api.github.com/users/", username, "/followers?per_page=100&page=", i),getToken)
+    followersContent <- content(followers)
+    currentFollowersDF <- lapply(followersContent, function(x) 
+    {
+      df <- data_frame(user = x$login, userID = x$id, followersURL = x$followers_url, followingURL = x$following_url)
+    }) %>% bind_rows()
+    i <- i+1
+    x <- length(followersContent)
+    followersDF <- rbind(followersDF, currentFollowersDF)
+  }
+  return (followersDF)
+}
+
+
+
+
+
+
+
+
+
 
 # Use API
-gtoken = config(token = github_token)
-UserFollowingData = GET("https://api.github.com/users/mbostock/following", gtoken)
+
+UserFollowingData = GET(paste0("https://api.github.com/users/mbostock/followers?per_page=100&page=",i), gtoken)
 UserFollowingDataContent = content(UserFollowingData)
 UserFollowingDataFrame = jsonlite::fromJSON(jsonlite::toJSON(UserFollowingDataContent))
 
 followersLogins = c(UserFollowingDataFrame$login)
+followersLogins
 
 # Extract content from a request
 json1 = content(req)
@@ -31,4 +64,4 @@ json1 = content(req)
 gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
 
 # Subset data.frame
-gitDF[gitDF$full_name == "stephenoquigley/datasharing", "created_at"] 
+gitDF[gitDF$full_name == "mbostock/datasharing", "created_at"] 
