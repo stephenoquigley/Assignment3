c6d96c642dfd3c48270f95be2f88d22104a6d034
Stephen O'Quigley <stephenoquigley@yahoo.ie>
2018-12-02 13:50:43 +0000

    Adding in comments

--- GithubAPI.R
+++ GithubAPI.R
@@ -20,6 +20,21 @@ myapp = oauth_app(appname = "APIgithub",
 # Get OAuth credentials
 github_token = oauth2.0_token(oauth_endpoints("github"), myapp)
 gtoken = config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+
+# Take action on http error
+stop_for_status(req)
+
+# Extract content from a request
+json1 = content(req)
+
+# Convert to a data.frame
+gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
+
+# Subset data.frame
+gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
+
+
 
 #-------------Interrogate the GitHub API to build visualisation of data available that--------------
 #-------------elucidates some aspect of the softare engineering process-----------------------------
